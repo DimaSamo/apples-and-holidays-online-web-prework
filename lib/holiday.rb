86439a5b0ec35110168ddc56c2717cca6a42298b
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-11 01:35:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       if supplies.include?("BBQ")
-        ret_array.push(holiday)
+        ret_array.push(:holiday)
       end
     end
     ret_array
